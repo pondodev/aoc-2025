e2706cff8f4cc3b19f249a8dbb219768d7436e25
Daniel Coady <me@pondo.dev>
2025-12-08 18:31:11 +1100

    day 6 part 2

--- src/t06.rs
+++ src/t06.rs
@@ -12,12 +12,13 @@ fn read_input_file() -> Vec<String> {
     result
 }
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug)]
 enum Operation {
     Add,
     Multiply,
 }
 
+#[derive(Clone, Debug)]
 struct Equation {
     nums:   Vec<u64>,
     op:     Operation,
@@ -63,6 +64,45 @@ impl Equation {
 
         to_return
     }
+
+    fn new_again(data: Vec<String>) -> Vec<Self> {
+        let ops = data.last().unwrap().chars().collect::<Vec<char>>();
+        let num_rows = data[..data.len()-1]
+            .iter()
+            .map(|l| l.chars().collect::<Vec<char>>())
+            .collect::<Vec<Vec<char>>>();
+
+        let mut to_return = vec![];
+        let mut working_equation = Self { nums: vec![], op: Operation::Add };
+        for cursor in 0..ops.len() {
+            match ops[cursor] {
+                '+' => {
+                    to_return.push(working_equation.clone());
+                    working_equation = Self { nums: vec![], op: Operation::Add };
+                },
+                '*' => {
+                    to_return.push(working_equation.clone());
+                    working_equation = Self { nums: vec![], op: Operation::Multiply };
+                },
+                _   => {},
+            }
+
+            let num_res = num_rows.iter()
+                .map(|r| r[cursor])
+                .collect::<String>()
+                .trim()
+                .parse::<u64>();
+
+            if let Ok(num) = num_res {
+                working_equation.nums.push(num);
+            }
+        }
+
+        // first equation will be invalid, so we replace it with the trailing equation
+        to_return[0] = working_equation;
+
+        to_return
+    }
 }
 
 pub fn p1() {
@@ -84,8 +124,17 @@ pub fn p1() {
 
 pub fn p2() {
     let data = read_input_file();
+    let equations = Equation::new_again(data);
 
-    let answer = 0;
+    let answer = equations.into_iter()
+        .fold(0, |acc, e| {
+            let res = match e.op {
+                Operation::Add => e.nums.into_iter().reduce(|total, n| total+n),
+                Operation::Multiply => e.nums.into_iter().reduce(|total, n| total*n),
+            }.unwrap();
+
+            res+acc
+        });
 
     println!("t06p2: {}", answer);
 }
